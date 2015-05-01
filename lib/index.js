krakenjs/swaggerize-express

moved app.api under app.swagger.api, removed setHost, added app.swagger.routes

lib/index.js
@@ -40,18 +40,14 @@ function swaggerize(options) {
  * @returns {onmount}
  */
 function mount(options) {
+
     return function onmount(parent) {
         parent._router.stack.pop();
 
-        Object.defineProperty(parent, 'api', {
-            enumerable: false,
-            value: options.api
-        });
-
-        Object.defineProperty(parent, 'setHost', {
-            enumerable: true,
-            value: function (value) {
-                options.api.host = value;
+        Object.defineProperty(parent, 'swagger', {
+            value: {
+                api: options.api,
+                routes: options.routes
             }
         });
 

test/test-swaggerize.js
@@ -20,17 +20,16 @@ test('swaggerize', function (t) {
     app.use(swagger);
 
     t.test('api', function (t) {
-        t.plan(5);
+        t.plan(6);
 
-        t.ok(app.hasOwnProperty('api'), 'has api property.');
-        t.ok(app.api, 'api is an object.');
+        t.ok(app.hasOwnProperty('swagger'), 'app has swagger property.');
+        t.ok(app.swagger, 'swagger is an object.');
 
-        t.ok(app.hasOwnProperty('setHost'), 'has setHost property.');
-        t.strictEqual(typeof app.setHost, 'function', 'setHost is a function.');
+        t.ok(app.swagger.hasOwnProperty('api'), 'app.swagger has api property.');
+        t.ok(app.swagger.api, 'app.swagger.api is an object.');
 
-        app.setHost('localhost:8080');
-
-        t.strictEqual(app.api.host, 'localhost:8080');
+        t.ok(app.swagger.hasOwnProperty('routes'), 'app.swagger has routes property.');
+        t.ok(app.swagger.routes, 'app.swagger.routes is an object.');
     });
 
     t.test('api as path', function (t) {

rochal/swaggerize-restify

Conflicts:
	CHANGELOG.md
	README.md
	lib/index.js
	package.json
	test/test-swaggerize.js

lib/index.js
@@ -34,20 +34,16 @@ function swaggerize(server, options) {
  * @param options
  * @returns {onmount}
  */
+
 function mount(server, options) {
-        Object.defineProperty(server, 'api', {
-            enumerable: false,
-            value: options.api
-        });
+    Object.defineProperty(server, 'swagger', {
+        value: {
+            api: options.api,
+            routes: options.routes
+        }
+    });
 
-        Object.defineProperty(server, 'setHost', {
-            enumerable: true,
-            value: function (value) {
-                options.api.host = value;
-            }
-        });
-
-        restifyroutes(server, options);
+    restifyroutes(server, options);
 }
 
 /**

test/test-swaggerize.js
@@ -20,17 +20,16 @@ test('swaggerize', function (t) {
 
 
     t.test('api', function (t) {
-        t.plan(5);
+        t.plan(6);
 
-        t.ok(server.hasOwnProperty('api'), 'has api property.');
-        t.ok(server.api, 'api is an object.');
+        t.ok(server.hasOwnProperty('swagger'), 'app has swagger property.');
+        t.ok(server.swagger, 'swagger is an object.');
 
-        t.ok(server.hasOwnProperty('setHost'), 'has setHost property.');
-        t.strictEqual(typeof server.setHost, 'function', 'setHost is a function.');
+        t.ok(server.swagger.hasOwnProperty('api'), 'app.swagger has api property.');
+        t.ok(server.swagger.api, 'app.swagger.api is an object.');
 
-        server.setHost('localhost:8080');
-
-        t.strictEqual(server.api.host, 'localhost:8080');
+        t.ok(server.swagger.hasOwnProperty('routes'), 'app.swagger has routes property.');
+        t.ok(server.swagger.routes, 'app.swagger.routes is an object.');
     });
 
     t.test('api as path', function (t) {